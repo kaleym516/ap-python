7757cd72ea69025d7824365566b74feca8f95426
kaleym516 <149606284+kaleym516@users.noreply.github.com>
2023-11-03 16:52:54 +0000

    school work

---
+++ kaleym program.py
@@ -0,0 +1,8 @@
+def my_function():
+    fname=input('whats your name? ')
+    pet =input('name your pet? ')
+    print('my pet name is '+ pet)
+    print("Hello "+ fname)
+print(5*5)
+my_function() 
+print
