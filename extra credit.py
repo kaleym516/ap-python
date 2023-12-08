a7bc8d3eb63f3cc219f7e70cf315414b66b0a3dd
kaleym516 <149606284+kaleym516@users.noreply.github.com>
2023-12-08 18:30:42 +0000

    work for extra credit

---
+++ extra credit.py
@@ -0,0 +1,17 @@
+print('Welcome to the movie guessing game')  
+print('this is a movie guessing game you will have 5 tries to guess the right movie')
+movieAnswer='jumanji' 
+print(movieAnswer)
+userAnxswer= input('a movie that takes place in a forest ')      
+print(userAnxswer)
+userAnxswer=print('incorrent')
+print=('incorrent you have 4 more tries to get this right')
+userAnxswer= input('something that involes a board game' )
+print (userAnxswer)
+
+
+# You need to make a Pyhton conditional statement that compares your user's movie guess to the correct answer
+    # do this
+    # do somthing else.
+
+# You need to use a loop to repeat the code, in this case the clues, until either the guess the right answer or exhaust all of their attempts
